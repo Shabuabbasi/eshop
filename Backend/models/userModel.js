51a61544830aca5814f696ae332e0bf6fc84f2e8
AbdulSattarMajid <malikabdulsattarmajid@gmail.com>
2025-07-27 10:40:43 +0500

    pushing backend

---
+++ Backend/models/userModel.js
@@ -0,0 +1,44 @@
+// userModel.js
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['Customer', 'Seller', 'Courier', 'Admin'], 
+    default: 'Customer',
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+  resetPasswordToken: String,
+resetPasswordExpires: Date,
+
+});
+
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
