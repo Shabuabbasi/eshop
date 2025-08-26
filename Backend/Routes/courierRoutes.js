e83d3ee34ce9db083568515f54b40165579d5daf
Salahudin <shabuabbasi456@gmail.com>
2025-08-26 14:43:39 +0500

    Update .gitignore to ignore sensitive and build files

--- Backend/Routes/courierRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAssignedOrders } from '../controllers/courierController.js';
-import { protect } from '../middlewares/authMiddleware.js';
-const router = express.Router();
-
-router.get('/assigned-orders', protect, getAssignedOrders);
-
-export default router;
